523fbd5a4ed4039cbf4d359f0a81a77b1a94be4e
李瑞娟 <1150482495@qq.com>
2020-11-12 16:57:07 +0800

    修改Test

--- src/test/java/com/xiaozhi/GitTest.java
+++ src/test/java/com/xiaozhi/GitTest.java
@@ -3,5 +3,7 @@ package com.xiaozhi;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("这里是git测试");
+        System.out.println("wwww");
+        System.out.println("dfser");
     }
 }
